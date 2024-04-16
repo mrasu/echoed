09fb1f8d75066112da78038918c07e23631eceb7
mrasu <m.rasu.hitsuji@gmail.com>
2024-04-16 23:06:54 +0900

    refactor: use submodule for opentelemetry-demo

--- create-echoed/tsup.config.ts
+++ create-echoed/tsup.config.ts
@@ -10,10 +10,18 @@ export default defineConfig({
     rm -rf ./dist/template &&
     rm -rf ./dist/template_tmp &&
     mkdir -p ./dist/template_tmp &&
-    git ls-files template | xargs -i cp --parents {} ./dist/template_tmp &&
+    git ls-files template | grep -v base/example/opentelemetry-demo | xargs -i cp --parents {} ./dist/template_tmp &&
+    cp --parents -r template/base/example/opentelemetry-demo ./dist/template_tmp &&
+    cp --parents -r template/base/example/opentelemetry-demo-override ./dist/template_tmp &&
+    rm -r ./dist/template_tmp/template/base/example/opentelemetry-demo/test &&
+    rm -r ./dist/template_tmp/template/base/example/opentelemetry-demo/src/frontend/cypress &&
+    rm ./dist/template_tmp/template/base/example/opentelemetry-demo/src/frontend/cypress.config.ts &&
+    cp -Tr ./dist/template_tmp/template/base/example/opentelemetry-demo-override ./dist/template_tmp/template/base/example/opentelemetry-demo &&
+    rm -r ./dist/template_tmp/template/base/example/opentelemetry-demo-override &&
     mv ./dist/template_tmp/template ./dist &&
     rmdir ./dist/template_tmp &&
-    mv ./dist/template/jest/_gitignore ./dist/template/jest/.gitignore
+    mv ./dist/template/cypress/_gitignore ./dist/template/cypress/.gitignore &&
+    mv ./dist/template/jest/_gitignore ./dist/template/jest/.gitignore &&
     mv ./dist/template/playwright/_gitignore ./dist/template/playwright/.gitignore
   `,
 });
